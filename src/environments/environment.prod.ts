f94862a3c141350eaa2c085fa9fa88df749160f6
ajinas3 <ajinas3@gmail.com>
2022-01-30 01:59:53 +0530

    updated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  authBaseUrl: 'http://localhost:8000/auth',
+  newsBaseUrl: 'https://api.nytimes.com/svc',
+  apiKey: '4azZci7OGlt0DlIBDxR259tUmnFcYP1T'
 };
